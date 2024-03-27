84da31c9f7fa3f956736c61ca036358e6a74c38f
parajuli rahul <parajulirahul98@gmail.com>
2024-03-27 17:39:49 +0545

    logic on chatcontroller& creating web config for socket config,UI forthe chating group and with js file

--- src/main/java/com/websocket/websocket/config/WebSocketEventListener.java
+++ src/main/java/com/websocket/websocket/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.websocket.websocket.config;
 
+import com.websocket.websocket.chat.ChatMessage;
+import com.websocket.websocket.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,13 +15,24 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
-
+private final SimpMessageSendingOperations messageTemplate;
 
     @EventListener
     public void handleWebSocketDisconnectListener(
             SessionDisconnectEvent event
             ){
-        //TODO--tobe implement
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username=(String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null){
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        } else {
+            log.warn("Disconnected user has no username attribute.");
+        }
     }
 
 }
